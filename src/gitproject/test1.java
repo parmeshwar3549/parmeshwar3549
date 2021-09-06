89adc7bd081c4e83d21df33385de1ad792f2c3d5
parmeshwar <parameshwarjj@gmail.com>
2021-09-06 13:35:52 +0530

    but commite

--- src/gitproject/test1.java
+++ src/gitproject/test1.java
@@ -3,7 +3,9 @@ package gitproject;
 public class test1 {
 	public static void main(String[] args) {
 		System.out.println("git project practice");
-		System.out.println("git project practice");
+		System.out.println("git project practice"); 
+		System.out.println("gitt project practice"); 
+		
 
 }
 }
